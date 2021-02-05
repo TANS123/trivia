62cd932d5e4d4557733cc6d10f2d197ab860ad99
TANS123 <44278831+TANS123@users.noreply.github.com>
2021-02-04 18:02:21 -0800

    did day 1 goals

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,22 @@ import "../css/App.css";
 import data from "../sample_data.json";
 
 function App() {
-  return <div className="app">Trivia!</div>;
+  var questionNumber = 0;
+  return (
+    <div className="app">
+      Trivia!
+      <Question text={data[questionNumber]["question"]["text"]} />
+      <NextQuestion />
+    </div>
+  );
+}
+
+function Question(props) {
+  return <div>{props.text}</div>;
+}
+
+function NextQuestion() {
+  return <button>Next Question</button>;
 }
 
 export default App;
