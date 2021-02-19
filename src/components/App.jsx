253b6ad62d8637a710fccc4f089fbf3705ee54b2
TANS123 <44278831+TANS123@users.noreply.github.com>
2021-02-18 17:57:22 -0800

    we finished day 2 goals and we made correct answer button

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,15 +3,24 @@ import "../css/App.css";
 import data from "../sample_data.json";
 
 function App() {
+  const [answerDisplayed, setAnswerDisplayed] = useState(true);
+  // let answerDisplayed = false;
   var questionNumber = 0;
   return (
     <div className="app">
       Trivia!
-      <Question
-        text={data[questionNumber]["question"]["text"]}
-        answerChoices={data[questionNumber]["question"]["choices"]}
-      />
-      <NextQuestion />
+      <div>
+        <Question
+          text={data[questionNumber]["question"]["text"]}
+          answerChoices={data[questionNumber]["question"]["choices"]}
+        />
+      </div>
+      <div>
+        <CorrectAnswer shouldDisplay={answerDisplayed} />
+      </div>
+      <div>
+        <NextQuestion />
+      </div>
     </div>
   );
 }
@@ -25,12 +34,35 @@ function Question(props) {
   );
 }
 
+function CorrectAnswer(props) {
+  let answerText;
+  if (props.shouldDisplay) {
+    answerText = "the answer";
+  } else {
+    answerText = "unanswered";
+  }
+
+  return (
+    <div>
+      <button>Click for the correct answer</button>
+      <div>The correct answer is {answerText}</div>
+    </div>
+  );
+}
+
 function NextQuestion() {
   return <button>Next Question</button>;
 }
 
 function Answer(props) {
-  return <div>{props.answerChoices}</div>;
+  return (
+    <div>
+      <div>{props.answerChoices[0]}</div>
+      <div>{props.answerChoices[1]}</div>
+      <div>{props.answerChoices[2]}</div>
+      <div>{props.answerChoices[3]}</div>
+    </div>
+  );
 }
 
 export default App;
